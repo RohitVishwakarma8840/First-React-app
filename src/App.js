cb54c656ffe4273d003dc71a33809d58dbc1f3c9
Rohit Vishwakarma <rohit8840@gmail.com>
2024-01-30 09:41:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+let name="Rohit Coder";
+const rohit_object={ name:"rv",work:"engineer"   };
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>hello rohit this side s</h1>
+<nav className="blank"> 
+<li>Home </li>
+<li>About</li>
+<li>Contact</li>
+<h1>{rohit_object.name}</h1>
+<h1>{5+5}</h1>
+<h1>hello this is {name} </h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ab nemo quia.</p>
+
+</nav>
+
+
+    </>
   );
 }
 
